3f42cf7c0b10bb6d3d9d961e82db22026e3f1f66
Aleyna Ceylan <aceylan1@binghamton.edu>
2021-09-12 20:32:56 -0400

    Variables assigned

--- hello/hello/ViewController.swift
+++ hello/hello/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var sizeButton: UIButton!
+    @IBOutlet weak var colorButton: UIButton!
+    @IBOutlet weak var magicButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
